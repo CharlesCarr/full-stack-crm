3cecb7de86e649ef49726ce3feb1e626b25d5b12
Charlie Carr <57736161+CharlesCarr@users.noreply.github.com>
2022-11-09 13:15:16 -0500

    feat: add update project components

--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -1,36 +1,39 @@
-import {Link, useParams } from 'react-router-dom';
-import Spinner from '../components/Spinner';
+import { Link, useParams } from "react-router-dom";
+import Spinner from "../components/Spinner";
 import { useQuery } from "@apollo/client";
-import { GET_PROJECT } from '../queries/projectQueries';
-import ClientInfo from '../components/ClientInfo';
-import DeleteProjectButton from '../components/DeleteProjectButton';
+import { GET_PROJECT } from "../queries/projectQueries";
+import ClientInfo from "../components/ClientInfo";
+import DeleteProjectButton from "../components/DeleteProjectButton";
+import EditProjectForm from "../components/EditProjectForm";
 
 const Project = () => {
-    const { id } = useParams();
-    const { loading, error, data} = useQuery(GET_PROJECT, { variables: { id }});
+  const { id } = useParams();
+  const { loading, error, data } = useQuery(GET_PROJECT, { variables: { id } });
 
-    if (loading) return <Spinner />;
-    if (error) return <p>Something went wrong...</p>
+  if (loading) return <Spinner />;
+  if (error) return <p>Something went wrong...</p>;
 
   return (
     <div className="w-full flex flex-col justify-center items-center">
-        {!loading && !error && (
-            <div className="border py-4">
-                <Link to="/">Back</Link>
+      {!loading && !error && (
+        <div className="border py-4">
+          <Link to="/">Back</Link>
 
-                <h1 className="font-bold text-2xl">{data.project.name}</h1>
-                <p>{data.project.description}</p>
+          <h1 className="font-bold text-2xl">{data.project.name}</h1>
+          <p>{data.project.description}</p>
 
-                <p>Project Status</p>
-                <p>{data.project.status}</p>
+          <p>Project Status</p>
+          <p>{data.project.status}</p>
 
-                <ClientInfo client={data.project.client} />
+          <ClientInfo client={data.project.client} />
 
-                <DeleteProjectButton projectId={data.project.id} />
-            </div>
-        )}
+          <EditProjectForm project={data.project} />
+
+          <DeleteProjectButton projectId={data.project.id} />
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Project
+export default Project;
