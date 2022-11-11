eac570d1def31faa559a6cf4c026c05c139ebd3c
Charlie Carr <57736161+CharlesCarr@users.noreply.github.com>
2022-11-10 23:26:36 -0500

    fix: change title of interactions page

--- client/src/pages/InteractionsPage.jsx
+++ client/src/pages/InteractionsPage.jsx
@@ -4,7 +4,7 @@ import Interactions from "../components/interactions/Interactions";
 const ProspectsPage = () => {
   return (
     <div className="w-full h-screen flex flex-col justify-start items-center pt-20">
-      <h1 className="font-bold text-4xl mb-8">Prospects Page</h1>
+      <h1 className="font-bold text-4xl mb-8">Interactions Page</h1>
       <AddInteractionModal />
       <Interactions />
     </div>
